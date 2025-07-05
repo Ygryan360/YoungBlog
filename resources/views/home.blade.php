0e3ed1d68cfc34015810d7ae267055762f0e1e37
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-07-05 00:20:40 +0000

    fix: Adjust font size for post title in post card component; refine text styling in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 @section('content')
     <div class="mb-8 border-b border-base-200 pb-4">
         <h1 class="text-3xl font-bold mb-1 text-white">Bienvenue sur YoungBlog</h1>
-        <p class="text-base-content">Découvrez mes dernières publications.</p>
+        <p>Découvrez mes dernières publications.</p>
     </div>
     {{--    <livewire:posts-carousel /> --}}
     <livewire:posts-container />
