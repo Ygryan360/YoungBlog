a5f9237741be72890fad80a17f2044d30a027e9d
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-09-04 22:59:56 +0000

    Refactor footer and guest layout styles for improved consistency and visual clarity

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+<footer class="footer sm:footer-horizontal footer-center border-t border-base-content/30 mt-8 text-base-content p-4">
     <aside>
         <livewire:newsletter-form />
         <p>Copyright © {{ date('Y') }} - YoungBlog</p>
